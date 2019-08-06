4b8d3ce634ef9da31b70f5c53ca8f954be0d5b25
nbstsh <nbstsh@gmail.com>
2019-08-07 00:23:35 +0900

    Using async/await with prisma bindings

--- src/prisma.js
+++ src/prisma.js
@@ -27,22 +27,84 @@ const prisma = new Prisma({
 // 		console.log(data);
 // 	});
 
-prisma.mutation
-	.createPost(
+// prisma.mutation
+// 	.createPost(
+// 		{
+// 			data: {
+// 				title: 'My name is John.',
+// 				body: 'Hello there, I am John. Please call me jojojojohn.',
+// 				published: false,
+// 				author: {
+// 					connect: {
+// 						id: 'cjyzxz09000ca0815dgg9xp1l'
+// 					}
+// 				}
+// 			}
+// 		},
+// 		'{ title body published author { id name } }'
+// 	)
+// 	.then(data => {
+// 		console.log(data);
+// 	});
+
+const createPostForUser = async (authorId, data) => {
+	const post = await prisma.mutation.createPost(
 		{
 			data: {
-				title: 'My name is John.',
-				body: 'Hello there, I am John. Please call me jojojojohn.',
-				published: false,
+				...data,
 				author: {
 					connect: {
-						id: 'cjyzxz09000ca0815dgg9xp1l'
+						id: authorId
 					}
 				}
 			}
 		},
-		'{ title body published author { id name } }'
-	)
-	.then(data => {
-		console.log(data);
-	});
+		'{ id }'
+	);
+	const user = await prisma.query.user(
+		{
+			where: {
+				id: authorId
+			}
+		},
+		'{ id name email posts { id title body published } comments { id text } }'
+	);
+
+	return user;
+};
+
+const updatePostForUser = async (postId, data) => {
+	const updatedPost = await prisma.mutation.updatePost(
+		{
+			where: {
+				id: postId
+			},
+			data
+		},
+		'{ id author { id} }'
+	);
+	const user = await prisma.query.user(
+		{
+			where: {
+				id: updatedPost.author.id
+			}
+		},
+		'{ id name email posts { id title body published } comments { id text } }'
+	);
+
+	return user;
+};
+
+// createPostForUser('cjyzxz09000ca0815dgg9xp1l', {
+// 	title: 'my dream',
+// 	body: 'I dream .....',
+// 	published: false
+// }).then(user => {
+// 	console.log(user);
+// });
+
+updatePostForUser('cjyzywx5p00d908154xxkmdq6', {
+	published: true
+}).then(user => {
+	console.log(user);
+});
