bc61610d7569773338e0056bd6327bdafcae6dcd
nbstsh <nbstsh@gmail.com>
2019-08-09 02:05:54 +0900

    Enable relatoinship of PostToUser and CommentToUser generated by prisma

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,10 +1,3 @@
-const User = {
-	posts(parent, args, { db }, info) {
-		return db.dummyPosts.filter(post => post.author === parent.id);
-	},
-	comments(parent, args, { db }, info) {
-		return db.dummyComments.filter(comment => comment.user === parent.id);
-	}
-};
+const User = {};
 
 export default User;
