90a3726c0c4e8746842054c2860eaa67ef999bb7
nbstsh <nbstsh@gmail.com>
2019-08-12 12:59:16 +0900

    Integrate prisma into comment subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,22 +1,18 @@
 const Subscription = {
-	count: {
-		subscribe(parent, args, { pubsub }, info) {
-			let count = 0;
-			setInterval(() => {
-				count++;
-				pubsub.publish('count', {
-					count
-				});
-			}, 1000);
-			return pubsub.asyncIterator('count');
-		}
-	},
 	comment: {
-		subscribe(parent, { postId }, { db, pubsub }, info) {
-			const { dummyPosts } = db;
-			const post = dummyPosts.find(post => post.id === postId);
-			if (!post) throw new Error('Post with given id was not found!');
-			return pubsub.asyncIterator(`comment ${post.id}`); // Channel name is `comment ${post.id}`
+		subscribe(parent, { postId }, { prisma }, info) {
+			return prisma.subscription.comment(
+				{
+					where: {
+						node: {
+							post: {
+								id: postId
+							}
+						}
+					}
+				},
+				info
+			);
 		}
 	},
 	post: {
